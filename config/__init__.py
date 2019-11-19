945a9598fcea1b10b52bc948214272d2795e9dc5
lxj <lixj_zj@163.com>
2019-11-19 22:59:56 +0800

    init structure

---
+++ config/__init__.py
@@ -0,0 +1,13 @@
+"""
+ !/usr/bin/env python3.6
+ -*- coding: utf-8 -*-
+ --------------------------------
+ Description :
+ --------------------------------
+ @Time    : 2019/11/19 21:51
+ @File    : __init__.py
+ @Software: PyCharm
+ --------------------------------
+ @Author  : lixj
+ @contact : lixj_zj@163.com
+"""
---
+++ core/tests/__init__.py
@@ -0,0 +1,13 @@
+"""
+ !/usr/bin/env python3.6
+ -*- coding: utf-8 -*-
+ --------------------------------
+ Description :
+ --------------------------------
+ @Time    : 2019/11/19 22:11
+ @File    : __init__.py.py
+ @Software: PyCharm
+ --------------------------------
+ @Author  : lixj
+ @contact : lixj_zj@163.com
+"""
